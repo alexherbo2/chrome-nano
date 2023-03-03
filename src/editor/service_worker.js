280ccfc156a0e4a5344a095aae1677547ce73a60
Mathieu Ablasou <taupiqueur.kanto@gmail.com>
2023-03-03 21:56:26 +0100

    2023-03-03 21:56:26

--- src/editor/service_worker.js
+++ src/editor/service_worker.js
@@ -1,7 +1,7 @@
-// This module contains the popup service worker to run commands via messages.
+// This module contains the editor service worker to run commands via messages.
 //
-// Uses a long-lived connection for the popup lifetime.
-// This allows to determine when the popup shows up or goes away.
+// Uses a long-lived connection for the editor lifetime.
+// This allows to determine when the editor shows up or goes away.
 //
 // Action: https://developer.chrome.com/docs/extensions/reference/action/
 // Service workers: https://developer.chrome.com/docs/extensions/mv3/service_workers/
@@ -20,22 +20,22 @@ function createMenuItems() {
 }
 
 // Handles the initial setup when the extension is first installed.
-async function onInstall() {
+function onInstall() {
   createMenuItems()
 }
 
 // Handles the setup when the extension is updated to a new version.
-async function onUpdate(previousVersion) {
+function onUpdate(previousVersion) {
   createMenuItems()
 }
 
-// Handles a new connection when the popup shows up.
+// Handles a new connection when the editor shows up.
 function onConnect(port) {
   port.onDisconnect.addListener(onDisconnect)
   port.onMessage.addListener(onMessage)
 }
 
-// Handles disconnection when the popup goes away.
+// Handles disconnection when the editor goes away.
 function onDisconnect(port) {
 }
 
@@ -73,18 +73,27 @@ function editTextArea() {
   // Open a channel to communicate with the service worker.
   const port = chrome.runtime.connect({ name: 'editor' })
   const activeElement = document.activeElement
-  const boundSelection = activeElement.setSelectionRange.bind(activeElement, activeElement.selectionStart, activeElement.selectionEnd, activeElement.selectionDirection)
-  port.onMessage.addListener((result) => {
-    if (result.status === 0) {
-      activeElement.value = result.output
-      boundSelection()
-      activeElement.dispatchEvent(new Event('input'))
-    }
-  })
-  port.postMessage({
-    type: 'open',
-    input: activeElement.value
-  })
+
+  switch (activeElement.constructor) {
+    case HTMLInputElement:
+    case HTMLTextAreaElement:
+      const boundSelection = activeElement.setSelectionRange.bind(activeElement, activeElement.selectionStart, activeElement.selectionEnd, activeElement.selectionDirection)
+      port.onMessage.addListener((result) => {
+        if (result.status === 0) {
+          activeElement.value = result.output
+          boundSelection()
+          activeElement.dispatchEvent(new Event('input'))
+        }
+      })
+      port.postMessage({
+        type: 'open',
+        input: activeElement.value
+      })
+      break
+
+    default:
+      port.disconnect()
+  }
 }
 
 export default { createMenuItems, onInstall, onUpdate, onConnect, onAction, onMenuItemClicked }
